2c27d2abd6e0b9840942d852b93630b3368d28a3
Prajwal Prakash <prajwalpy6@gmail.com>
2020-06-13 01:33:59 -0400

    Use the Ambient Layer API to create an example layer

--- ambient-sandbox-application/src/app.cpp
+++ ambient-sandbox-application/src/app.cpp
@@ -1,8 +1,19 @@
 #include "ambient.h"
 
+class MyLayer : public Ambient::Layer {
+ public:
+  MyLayer() : Layer("My Layer") {}
+
+  void OnUpdate() override { AM_INFO("Called SampleLayer::OnUpdate"); }
+
+  void OnEvent(Ambient::Event::Event& event) override {
+    AM_TRACE("{0}", event.ToString());
+  }
+};
+
 class SandboxApp : public Ambient::Application {
  public:
-  SandboxApp() {}
+  SandboxApp() { PushLayer(new MyLayer()); }
   ~SandboxApp() {}
 };
 
